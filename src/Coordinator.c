e5d9354e6b72227d5c5ec7897a9c3e9e5e3b31c4
CSU-Ryan <enderbro@colostate.edu>
2026-02-19 16:09:33 -0700

    Rewrites Coordinator.c for new specs

--- src/Coordinator.c
+++ src/Coordinator.c
@@ -2,71 +2,183 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <sys/shm.h>
+
+#define CHECKER_COUNT 4
+
+
+/**
+ * Creates shared memory segments for all child processes
+ * @param ids list to be filled with shared memory ids
+ * @return 0 if success, 1 if failure
+ */
+int init_memory(int ids[]) {
+    for (int i = 0; i < CHECKER_COUNT; i++) {
+        const int shm_id = shmget(IPC_PRIVATE, sizeof (int), IPC_CREAT);
+
+        if (shm_id < 0) {
+            printf("Coordinator: shmget failed.\n");
+            return 1;
+        }
+
+        ids[i] = shm_id;
+    }
+    return 0;
+}
+
+/**
+ * Closes shared memory segments.
+ * @param ids shared memory ids
+ */
+void clean_memory(const int ids[]) {
+    for (int i = 0; i < CHECKER_COUNT; i++) {
+        shmctl(ids[i], IPC_RMID, NULL);
+    }
+}
+
+/**
+ * Creates pipes
+ * @param pipes array to have pipe FDs written to
+ * @return 0 on success, 1 on error
+ */
+int init_pipes(int pipes[][2]) {
+    for (int i = 0; i < CHECKER_COUNT; i++) {
+        if (pipe(pipes[i])) {
+            printf("Coordinator: pipe failed.\n");
+            return 1;
+        }
+    }
+    return 0;
+}
+
+/**
+ * Closes read-end of pipes.
+ * @param pipes List of pipe FDs
+ * @warning Only use when pipes have not been given to other threads.
+ */
+void clean_pipe_readers(int pipes[][2]) {
+    for (int i = 0; i < CHECKER_COUNT; i++) {
+        close(pipes[i][0]);
+    }
+}
+
+/**
+ * Closes write-end of pipes.
+ * @param pipes List of pipe FDs
+ */
+void clean_pipe_writers(int pipes[][2]) {
+    for (int i = 0; i < CHECKER_COUNT; i++) {
+        close(pipes[i][1]);
+    }
+}
+
+/**
+ * Closes all pipes and allocated memory
+ * @param memory_ids Shared memory id list
+ * @param pipes Pipe FD list
+ * @warning Only use when pipes have not been given to other threads.
+ */
+void quick_exit(int memory_ids[], int pipes[][2]) {
+    clean_memory(memory_ids);
+    clean_pipe_readers(pipes);
+    clean_pipe_writers(pipes);
+}
+
 
 /**
  * Makes current process run Checker code
  */
-void executeChecker(const int divisor, const int dividend) {
-    char divisorString[16];
-    sprintf(divisorString, "%d", divisor);
-    char dividendString[16];
-    sprintf(dividendString, "%d", dividend);
+void execute_checker(const int divisor, const int dividend, const int pipe_fd) {
+    char divisor_string[16];
+    sprintf(divisor_string, "%d", divisor);
+    char dividend_string[16];
+    sprintf(dividend_string, "%d", dividend);
+    char pipe_string[16];
+    sprintf(pipe_string, "%d", pipe_fd);
 
     execlp("bin/checker.o",
-        "bin/checker.o", divisorString, dividendString,
-        (char *)NULL);
+        "bin/checker.o", divisor_string, dividend_string, pipe_string,
+        (char *) NULL);
+}
+
+void write_pipe(const int write_fd, int data, int size) {
+
 }
 
 /**
- * Awaits the return value of the Checker child
+ * Expects 5 integer arguments: divisor, dividend1, ..., dividend4
+ * Runs a Checker process for each dividend, checking whether the divisor divides the dividend.
  */
-void receiveCheck(const int childpid) {
-    printf("Coordinator: forked process with ID %d.\n", childpid);
-    printf("Coordinator: waiting for process [%d].\n", childpid);
-
-    int status;
-    waitpid(childpid, &status, 0);
-
-    printf("Coordinator: child process %d returned %d.\n",
-        childpid, WEXITSTATUS(status)
-        );
-}
-
-/**
- * Expects 5 integer arguments: dividend, divisor1, ..., divisor4
- * Runs a Checker process for each divisor, checking whether the divisor divides the dividend.
- */
-int main(int argc, char **argv) {
-    if (argc != 6) {
-        printf("Coordinator: invalid number of arguments (%d =/= 6).\n", argc);
+int main(const int argc, char **argv) {
+    if (argc != CHECKER_COUNT + 2) {
+        printf("Coordinator: invalid number of arguments.\n");
         return 1;
     }
 
-    const int dividend = atoi(argv[1]);
-    const int divisors[4] = {atoi(argv[2]), atoi(argv[3]), atoi(argv[4]), atoi(argv[5])};
+    const int divisor = atoi(argv[1]);
+    int dividends[CHECKER_COUNT];
+    for (int i = 0; i < CHECKER_COUNT; i++) {
+        dividends[i] = atoi(argv[i + 2]);
+    }
 
-    // Loops over divisors
-    for (int i = 0; i < 4; i++) {
-        const int divisor = divisors[i];
+    int child_pids[CHECKER_COUNT];
+    int shm_ids[CHECKER_COUNT];
+    int pipes[CHECKER_COUNT][2];
 
+    // Initialize shared memory
+    if (init_memory(shm_ids)) {
+        quick_exit(shm_ids, pipes);
+        return 1;
+    }
+
+    // Initialize pipes
+    if (init_pipes(pipes)) {
+        quick_exit(shm_ids, pipes);
+        return 1;
+    }
+
+    // Checker creation loop
+    for (int i = 0; i < CHECKER_COUNT; i++) {
+        const int dividend = dividends[i];
         const int pid = fork();
-        if (pid == 0) {
-            // Code for the Child
-            executeChecker(divisor, dividend);
 
-            printf("Coordinator: failed to execute checker.\n");
-            return 1;
-        }
-        else if (pid > 0) {
-            // Code for the Parent
-            receiveCheck(pid);
-        }
-        else {
+        if (pid < 0) {
             // Failed to fork
             printf("Coordinator: could not fork child process.\n");
-            return 1;
+            continue;
+        }
+
+        if (pid > 0) {
+            // Code for the Parent
+            child_pids[i] = pid;
+            printf("Coordinator: forked process with ID %d.\n", pid);
+
+            write(pipes[i][1], &shm_ids[i], sizeof shm_ids[i]);
+            printf("Coordinator: wrote shm ID %d to pipe (%llu bytes)",
+                shm_ids[i], sizeof shm_ids[i]);
+        }
+        else {
+            // Code for the Child
+            execute_checker(divisor, dividend, pipes[i][0]);
+
+            // Code should be unreachable
+            printf("Coordinator Child: failed to execute checker.\n");
         }
     }
+
+    for (int i = 0; i < CHECKER_COUNT; i++) {
+        printf("Coordinator: waiting on child process ID %d...\n", child_pids[i]);
+
+        int _;
+        waitpid(child_pids[i], &_, 0);
+
+        const int *shared_address = shmat(shm_ids[i], NULL, 0);
+        const int result = *shared_address;
+
+        printf("Coordinator: result %d read from shared memory: %d is divisible by %d.\n",
+            result, dividends[i], divisor);
+    }
+
 
     printf("Coordinator: exiting.\n");
 }
