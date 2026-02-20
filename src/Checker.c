347ed5017b51c7d9ea640ec111d4cb7c0067b012
CSU-Ryan <enderbro@colostate.edu>
2026-02-20 13:44:13 -0700

    Implements Checker.c

--- src/Checker.c
+++ src/Checker.c
@@ -1,39 +1,55 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/shm.h>
 
 /**
- * Expects 2 integer arguments: divisor and dividend.
- * Returns 1 if divisor divides dividend, and 0 otherwise
+ * Reads the shared memory ID from the given pipeFD.
+ * @param pipeFD File descriptor for read-end of pipe
+ * @return Shared memory ID from the pipe. If errors occur, returns -1.
+ */
+int get_shm_id(const int pipeFD) {
+    int pipe_read;
+    if (read(pipeFD, &pipe_read, sizeof (int)) <= 0) { return -1; }
+
+    return pipe_read;
+}
+
+/**
+ * Writes the given data to the specified shared memory
+ * @param shm_id ID for shared memory
+ * @param data data to be written
+ * @return 0 on successful write, 1 on fail.
+ */
+int write_shm(const int shm_id, const int data) {
+    int *shm = shmat(shm_id, NULL, 0);
+    if (shm == (int *)-1) { return 1; }
+    *shm = data;
+    shmdt(shm);
+
+    return 0;
+}
+
+/**
+ * Expects 3 integer arguments: divisor, dividend, and the read-end FD of a pipe.
+ * Writes whether divisor divides dividend to shared memory, with ID received from the pipe.
+ * @returns Returns 1 if divisor divides dividend, and 0 otherwise
  */
 int main(const int argc, char **argv) {
     if (argc != 4) {
-        printf("Checker process [%d]: Invalid number of arguments (%d =/= 4).\n", getpid(), argc);
+        printf("Checker process [%d]: Invalid number of arguments.\n", getpid());
         return 1;
     }
 
     const int divisor = atoi(argv[1]);
     const int dividend = atoi(argv[2]);
-	const int pipeFD = atoi(argv[3]);
-
-    printf("Checker process [%d]: Starting.\n", getpid());
+    const int pipe_FD = atoi(argv[3]);
 
     int divides = (dividend % divisor == 0);
 
-    if (divides) {
-        printf("Checker process [%d]: %d *IS* divisible by %d.\n",
-        getpid(),
-        dividend,
-        divisor
-        );
-    } else {
-        printf("Checker process [%d]: %d *IS NOT* divisible by %d.\n",
-        getpid(),
-        dividend,
-        divisor
-        );
-    }
-
+    const int shm_id = get_shm_id(pipe_FD);
+    if (shm_id == -1) { return 1; }
+    if (write_shm(shm_id, divides)) { return 1; }
 
     return 0;
 }
